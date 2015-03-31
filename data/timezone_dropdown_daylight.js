d3044f4d51f35c4bd482c0fb87fa8ebdcd185001
xemlock <xemlock@gmail.com>
2015-03-31 23:30:28 +0200

    updated webex timezone data files

---
+++ data/timezone_dropdown_daylight.js
@@ -0,0 +1,350 @@
+[
+    {
+        "value": "1",
+        "label": "Samoa (Samoa Time, GMT-11:00)"
+    },
+    {
+        "value": "2",
+        "label": "Honolulu (Hawaii Time, GMT-10:00)"
+    },
+    {
+        "value": "3",
+        "label": "Anchorage (Alaska Daylight Time, GMT-08:00)"
+    },
+    {
+        "value": "5",
+        "label": "Arizona (Mountain Time, GMT-07:00)"
+    },
+    {
+        "value": "132",
+        "label": "Chihuahua (Mexico Mountain Standard Time, GMT-07:00)"
+    },
+    {
+        "value": "4",
+        "label": "San Francisco (Pacific Daylight Time, GMT-07:00)"
+    },
+    {
+        "value": "131",
+        "label": "Tijuana (Mexico Pacific Daylight Time, GMT-07:00)"
+    },
+    {
+        "value": "6",
+        "label": "Denver (Mountain Daylight Time, GMT-06:00)"
+    },
+    {
+        "value": "8",
+        "label": "Mexico City (Mexico Standard Time, GMT-06:00)"
+    },
+    {
+        "value": "9",
+        "label": "Saskatchewan (Central Time, GMT-06:00)"
+    },
+    {
+        "value": "137",
+        "label": "Tegucigalpa (Honduras Time, GMT-06:00)"
+    },
+    {
+        "value": "10",
+        "label": "Bogota (S. America Pacific Time, GMT-05:00)"
+    },
+    {
+        "value": "7",
+        "label": "Chicago (Central Daylight Time, GMT-05:00)"
+    },
+    {
+        "value": "146",
+        "label": "Panama (Eastern Time, GMT-05:00)"
+    },
+    {
+        "value": "133",
+        "label": "Caracas (S. America Western Time, GMT-04:30)"
+    },
+    {
+        "value": "12",
+        "label": "Indiana (Eastern Daylight Time, GMT-04:00)"
+    },
+    {
+        "value": "14",
+        "label": "La Paz (S. America Western Time, GMT-04:00)"
+    },
+    {
+        "value": "11",
+        "label": "New York (Eastern Daylight Time, GMT-04:00)"
+    },
+    {
+        "value": "145",
+        "label": "Santiago (S. America Western Standard Time, GMT-04:00)"
+    },
+    {
+        "value": "148",
+        "label": "Asuncion (Paraguay Summer Time, GMT-03:00)"
+    },
+    {
+        "value": "16",
+        "label": "Brasilia (S. America Eastern Standard Time, GMT-03:00)"
+    },
+    {
+        "value": "17",
+        "label": "Buenos Aires (S. America Eastern Time, GMT-03:00)"
+    },
+    {
+        "value": "13",
+        "label": "Halifax (Atlantic Daylight Time, GMT-03:00)"
+    },
+    {
+        "value": "149",
+        "label": "Montevideo (Uruguay Time, GMT-03:00)"
+    },
+    {
+        "value": "135",
+        "label": "Recife (S. America Eastern Time, GMT-03:00)"
+    },
+    {
+        "value": "15",
+        "label": "Newfoundland (Newfoundland Daylight Time, GMT-02:30)"
+    },
+    {
+        "value": "18",
+        "label": "Mid-Atlantic (Mid-Atlantic Time, GMT-02:00)"
+    },
+    {
+        "value": "138",
+        "label": "Nuuk (Greenland Daylight Time, GMT-02:00)"
+    },
+    {
+        "value": "19",
+        "label": "Azores (Azores Time, GMT-01:00)"
+    },
+    {
+        "value": "150",
+        "label": "Cape Verde (Cape Verde Time, GMT-01:00)"
+    },
+    {
+        "value": "136",
+        "label": "Casablanca (Morocco Standard Time, GMT)"
+    },
+    {
+        "value": "21",
+        "label": "London (GMT Time, GMT)"
+    },
+    {
+        "value": "20",
+        "label": "Reykjavik (Greenwich Time, GMT)"
+    },
+    {
+        "value": "143",
+        "label": "West Africa (West Africa Time, GMT+01:00)"
+    },
+    {
+        "value": "22",
+        "label": "Amsterdam (Europe Summer Time, GMT+02:00)"
+    },
+    {
+        "value": "25",
+        "label": "Berlin (Europe Summer Time, GMT+02:00)"
+    },
+    {
+        "value": "147",
+        "label": "Brussels (Europe Summer Time, GMT+02:00)"
+    },
+    {
+        "value": "28",
+        "label": "Cairo (Egypt Time, GMT+02:00)"
+    },
+    {
+        "value": "144",
+        "label": "Madrid (Europe Summer Time, GMT+02:00)"
+    },
+    {
+        "value": "23",
+        "label": "Paris (Europe Summer Time, GMT+02:00)"
+    },
+    {
+        "value": "29",
+        "label": "Pretoria (South Africa Time, GMT+02:00)"
+    },
+    {
+        "value": "142",
+        "label": "Rome (Europe Summer Time, GMT+02:00)"
+    },
+    {
+        "value": "130",
+        "label": "Stockholm (Sweden Summer Time, GMT+02:00)"
+    },
+    {
+        "value": "151",
+        "label": "Windhoek (West Africa Summer Time, GMT+02:00)"
+    },
+    {
+        "value": "139",
+        "label": "Amman (Jordan Daylight Time, GMT+03:00)"
+    },
+    {
+        "value": "26",
+        "label": "Athens (Greece Summer Time, GMT+03:00)"
+    },
+    {
+        "value": "30",
+        "label": "Helsinki (Northern Europe Summer Time, GMT+03:00)"
+    },
+    {
+        "value": "140",
+        "label": "Istanbul (Eastern Europe Summer Time, GMT+03:00)"
+    },
+    {
+        "value": "33",
+        "label": "Moscow (Russian Time, GMT+03:00)"
+    },
+    {
+        "value": "34",
+        "label": "Nairobi (Nairobi Time, GMT+03:00)"
+    },
+    {
+        "value": "32",
+        "label": "Riyadh (Saudi Arabia Time, GMT+03:00)"
+    },
+    {
+        "value": "31",
+        "label": "Tel Aviv (Israel Daylight Time, GMT+03:00)"
+    },
+    {
+        "value": "36",
+        "label": "Abu Dhabi, Muscat (Arabian Time, GMT+04:00)"
+    },
+    {
+        "value": "155",
+        "label": "Yerevan (Armenia Time, GMT+04:00)"
+    },
+    {
+        "value": "38",
+        "label": "Kabul (Afghanistan Time, GMT+04:30)"
+    },
+    {
+        "value": "35",
+        "label": "Tehran (Iran Daylight Time, GMT+04:30)"
+    },
+    {
+        "value": "37",
+        "label": "Baku (Baku Daylight Time, GMT+05:00)"
+    },
+    {
+        "value": "40",
+        "label": "Islamabad (West Asia Time, GMT+05:00)"
+    },
+    {
+        "value": "39",
+        "label": "Yekaterinburg (West Asia Time, GMT+05:00)"
+    },
+    {
+        "value": "42",
+        "label": "Colombo (Colombo Time, GMT+05:30)"
+    },
+    {
+        "value": "41",
+        "label": "Mumbai (India Time, GMT+05:30)"
+    },
+    {
+        "value": "141",
+        "label": "Kathmandu (Nepal Time, GMT+05:45)"
+    },
+    {
+        "value": "43",
+        "label": "Almaty (Central Asia Time, GMT+06:00)"
+    },
+    {
+        "value": "156",
+        "label": "Novosibirsk (Novosibirsk Time, GMT+06:00)"
+    },
+    {
+        "value": "152",
+        "label": "Yangon (Myanmar Time, GMT+06:30)"
+    },
+    {
+        "value": "44",
+        "label": "Bangkok (Bangkok Time, GMT+07:00)"
+    },
+    {
+        "value": "154",
+        "label": "Jakarta (Western Indonesia Time, GMT+07:00)"
+    },
+    {
+        "value": "45",
+        "label": "Beijing (China Time, GMT+08:00)"
+    },
+    {
+        "value": "134",
+        "label": "Kuala Lumpur (Malaysia Time, GMT+08:00)"
+    },
+    {
+        "value": "46",
+        "label": "Perth (Australia Western Time, GMT+08:00)"
+    },
+    {
+        "value": "47",
+        "label": "Singapore (Singapore Time, GMT+08:00)"
+    },
+    {
+        "value": "48",
+        "label": "Taipei (Taipei Time, GMT+08:00)"
+    },
+    {
+        "value": "50",
+        "label": "Seoul (Korea Time, GMT+09:00)"
+    },
+    {
+        "value": "49",
+        "label": "Tokyo (Japan Time, GMT+09:00)"
+    },
+    {
+        "value": "51",
+        "label": "Yakutsk (Yakutsk Time, GMT+09:00)"
+    },
+    {
+        "value": "53",
+        "label": "Darwin (Australia Central Time, GMT+09:30)"
+    },
+    {
+        "value": "54",
+        "label": "Brisbane (Australia Eastern Time, GMT+10:00)"
+    },
+    {
+        "value": "56",
+        "label": "Guam (West Pacific Time, GMT+10:00)"
+    },
+    {
+        "value": "58",
+        "label": "Vladivostok (Vladivostok Time, GMT+10:00)"
+    },
+    {
+        "value": "52",
+        "label": "Adelaide (Australia Central Daylight Time, GMT+10:30)"
+    },
+    {
+        "value": "57",
+        "label": "Hobart (Tasmania Daylight Time, GMT+11:00)"
+    },
+    {
+        "value": "59",
+        "label": "Solomon Is (Central Pacific Time, GMT+11:00)"
+    },
+    {
+        "value": "55",
+        "label": "Sydney (Australia Eastern Daylight Time, GMT+11:00)"
+    },
+    {
+        "value": "61",
+        "label": "Fiji (Fiji Standard Time, GMT+12:00)"
+    },
+    {
+        "value": "0",
+        "label": "Marshall Islands (Dateline Time, GMT+12:00)"
+    },
+    {
+        "value": "153",
+        "label": "Tonga (Tonga Time, GMT+13:00)"
+    },
+    {
+        "value": "60",
+        "label": "Wellington (New Zealand Daylight Time, GMT+13:00)"
+    }
+]
